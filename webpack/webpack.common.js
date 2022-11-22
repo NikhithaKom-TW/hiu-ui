e6b48cb120c73a7df59897ffd52639ef22cc6348
swatigogia2020 <73937345+swatigogia2020@users.noreply.github.com>
2022-11-22 11:00:54 +0530

    BAH-2512: upgraded the node version (#17)
    * BAH-2512: upgraded the node version
    
    * BAH-2512: Upgrading the node version
    
    * upgrade node to 16.18.1
    
    Co-authored-by: _n0man <nouman.memon@icloud.com>

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,28 +1,30 @@
-var path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+var path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-var parentDir = path.join(__dirname, "../");
+var parentDir = path.join(__dirname, '../')
 
 module.exports = {
-  context: path.join(parentDir, ""),
+  context: path.join(parentDir, ''),
   plugins: [
-    new CopyWebpackPlugin([
-      { from: "static" },
-      { from: "index.html" },
-      { from: "dicomIndex.html" }
-    ])
+    new CopyWebpackPlugin({
+      patterns: [
+        {from: 'static'},
+        {from: 'index.html'},
+        {from: 'dicomIndex.html'},
+      ],
+    }),
   ],
-  entry: [path.join(parentDir, "index.js")],
+  entry: [path.join(parentDir, 'index.js')],
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+    rules: [{test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}],
   },
   resolve: {
     alias: {
-      Components: path.resolve(__dirname, "../src/components/")
-    }
+      Components: path.resolve(__dirname, '../src/components/'),
+    },
   },
   output: {
-    path: parentDir + "/dist",
-    filename: "bundle.js"
-  }
-};
+    path: parentDir + '/dist',
+    filename: 'bundle.js',
+  },
+}
